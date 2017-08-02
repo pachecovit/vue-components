f4e8d966180a65dca45ab685c7b89733cd8282de
Cleber Pacheco <cleber.pacheco@dotgroup.com.br>
2017-08-02 14:45:09 -0300

    Added text area form item.

---
+++ forms/InputTextArea.vue
@@ -0,0 +1,48 @@
+<script>
+  export default {
+    name: 'inputTextArea',
+    data () {
+      return {
+        isFocused: false,
+        objectStyle: {
+          height: 'auto'
+        }
+      }
+    },
+    props: {
+      placeholder: String,
+      label: String,
+      disabled: Boolean,
+      readonly: Boolean,
+      value: null,
+      rows: {
+        type: Number,
+        default: 6
+      },
+      maxRows: {
+        type: Number,
+        default: null
+      },
+      type: String
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <textarea
+      spellcheck="false"
+      :style="objectStyle"
+      :rows="rows"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      :readonly="readonly"
+      :value="value"
+      @input="updateValue($event.target.value)"
+    </textarea>
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
