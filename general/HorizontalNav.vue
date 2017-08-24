f3bcf2eab36ff98c639a005b79e34d6e08e052cb
Cleber Pacheco <cleber.pacheco@dotgroup.com.br>
2017-08-24 17:27:04 -0300

    Added horizontal nav component.

---
+++ general/HorizontalNav.vue
@@ -0,0 +1,40 @@
+<script>
+  export default {
+    name: 'horizontlNav',
+    components: {
+      Icon
+    },
+    props: {
+      items: {
+        type: Array,
+        default: () => {
+          return []
+        }
+      },
+      multiLevel: {
+        type: Boolean,
+        default: false
+      },
+      subItems: {
+        type: Array,
+        default: () => {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in this.items"><a href="item.link">{{ item.text }}</a>
+      <ul v-if="multiLevel">
+        <li v-for="subItem in this.subItems"><a href="subItem.link">{{ subItem.item }}</a></li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
