5b68800d151d79fb786754102a07fdbe07210a8d
Cleber Pacheco <cleber.pacheco@dotgroup.com.br>
2017-09-06 14:42:20 -0300

    Added datatable component using vuetable.

---
+++ general/modules/VuetablePagination.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="tablePagination && tablePagination.last_page > 1" :class="css.wrapperClass">
+    <a @click="loadPage(1)"
+      :class="['btn-nav', css.linkClass, isOnFirstPage ? css.disabledClass : '']">
+        First
+    </a>
+    <a @click="loadPage('prev')"
+      :class="['btn-nav', css.linkClass, isOnFirstPage ? css.disabledClass : '']">
+        Next
+    </a>
+    <template v-if="notEnoughPages">
+      <template v-for="n in totalPage">
+        <a @click="loadPage(n)"
+          :class="[css.pageClass, isCurrentPage(n) ? css.activeClass : '']"
+          v-html="n">
+        </a>
+      </template>
+    </template>
+    <template v-else>
+      <template v-for="n in windowSize">
+        <a @click="loadPage(windowStart+n-1)"
+          :class="[css.pageClass, isCurrentPage(windowStart+n-1) ? css.activeClass : '']"
+          v-html="windowStart+n-1">
+        </a>
+      </template>
+    </template>
+    <a @click="loadPage('next')"
+      :class="['btn-nav', css.linkClass, isOnLastPage ? css.disabledClass : '']">
+        Previous
+    </a>
+    <a @click="loadPage(totalPage)"
+      :class="['btn-nav', css.linkClass, isOnLastPage ? css.disabledClass : '']">
+        Last
+    </a>
+  </div>
+</template>
+
+<script>
+  import PaginationMixin from '@/general/modules/VuetablePaginationMixin.vue'
+
+  export default {
+    mixins: [PaginationMixin]
+  }
+</script>
